be5f8c1b364498792f035386fce8e462053df6c7
amankumarinfy <amankumarmsat@gmail.com>
2024-06-28 13:03:41 +0530

    test

--- src/test/java/StepDefinitions/capstone_Project_UI_EXCEL_DB_Step.java
+++ src/test/java/StepDefinitions/capstone_Project_UI_EXCEL_DB_Step.java
@@ -136,7 +136,7 @@ public class capstone_Project_UI_EXCEL_DB_Step {
 		
 		@Then("User launches external google ui website")
 		public void user_launches_google_ui() throws InterruptedException, IOException {
-			utils = new utils(driver);
+			//utils = new utils(driver);
 			hooks = new hooks();
 			driver = hooks.startBrowser_google_chrome_n();
 		}
